16a44c4f6639f3eb2e96cc008b627533b82d340d
d-bastian <bastian.dabernig@pos.ag>
2024-12-16 00:28:11 +0100

    adjusted settings

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,22 +1,36 @@
 -- Vim Settings
+
+-- Line Numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Encoding
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencodings = { 'utf-8', 'utf-8bom', 'latin1' }
-vim.opt.wrap = true
+
+-- Indentation
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+
+-- Search
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
+
+-- Display
+vim.opt.wrap = true
 vim.opt.mouse = 'a'
 vim.opt.clipboard = 'unnamedplus'
-vim.cmd('syntax enable')
-vim.cmd('filetype plugin on')
-vim.cmd.colorscheme("moonfly")
+vim.opt.termguicolors = true
+vim.opt.guicursor = ''
 vim.opt.fillchars:append({
     eob = " ", -- Replace tildes with blank space
 })
-vim.opt.termguicolors = true
+
+-- Commands
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin on')
+vim.cmd.colorscheme("moonfly")
