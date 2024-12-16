5453267f3cebd2a3af5b974effbe916e60d3f591
d-bastian <bastian.dabernig@pos.ag>
2024-12-16 01:03:55 +0100

    added telescopesetup

---
+++ lua/plugins/telescope-setup.lua
@@ -0,0 +1,12 @@
+require("telescope").setup({
+    defaults = {
+        layout_config = {
+            prompt_position = "top"
+        },
+        pickers = {
+            find_files = {
+                hidden = true
+            }
+        }
+    },
+})
