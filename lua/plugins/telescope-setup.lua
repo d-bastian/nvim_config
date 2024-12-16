5f9ad01e27118d5b9ae89484a88f9cc39bab0f5e
Bastian Dabernig <bastian.dabernig@pos.ag>
2024-12-16 11:17:38 +0100

    added current buffer find

--- lua/plugins/telescope-setup.lua
+++ lua/plugins/telescope-setup.lua
@@ -1,7 +1,7 @@
 require("telescope").setup({
     defaults = {
         layout_config = {
-            prompt_position = "top"
+            prompt_position = "bottom"
         },
         pickers = {
             find_files = {
