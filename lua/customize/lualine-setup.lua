dd3a4ee78ad3af25a275ef0dc1db64240492376a
Bastian Dabernig <bastian.dabernig@pos.ag>
2025-01-09 10:58:39 +0100

    custom lualine sep

--- lua/customize/lualine-setup.lua
+++ lua/customize/lualine-setup.lua
@@ -1,6 +1,7 @@
 local custom_theme = require("lualine.themes.codedark")
 require("lualine").setup {
-    options = { theme = custom_theme },
+    options = { theme = custom_theme, component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' }, },
     sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
