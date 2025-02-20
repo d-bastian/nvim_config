fc0fbe56921c95ada9d3e33f8d81cffbb631d972
d-bastian <bastian.dabernig@pos.ag>
2025-02-20 22:27:34 +0100

    Added new themes

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -48,7 +48,7 @@ vim.opt.fillchars:append({
 })
 
 -- Theme
-vim.cmd.colorscheme("moonfly")
+vim.cmd.colorscheme("tairiki")
 
 -- Auto format on save
 vim.cmd([[
--- lua/config/themes.lua
+++ lua/config/themes.lua
@@ -4,9 +4,28 @@ return {
     { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
     "nyoom-engineering/oxocarbon.nvim",
     "oonamo/ef-themes.nvim",
-    { 'kepano/flexoki-neovim',     name = 'flexoki' },
+    { "kepano/flexoki-neovim",     name = "flexoki" },
     {
         "olimorris/onedarkpro.nvim",
         priority = 1000, -- Ensure it loads first
+    },
+    {
+        "Verf/deepwhite.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "deparr/tairiki.nvim",
+        lazy = false,
+        priority = 1000, -- only necessary if you use tairiki as default theme
+        config = function()
+            require("tairiki").setup {
+                style = "dark",
+                transparent = true,
+                lualine = {
+                    transparent = true,
+                }
+            }
+        end,
     }
 }
