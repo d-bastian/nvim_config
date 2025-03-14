aca0904825268430ba0ec93c5eae0c0409ade4eb
Bastian Dabernig <bastian.dabernig@pos.ag>
2025-03-14 14:11:03 +0100

    Removed unused themes

--- lua/config/themes.lua
+++ lua/config/themes.lua
@@ -1,42 +1,9 @@
 return {
-    "rebelot/kanagawa.nvim",
-    "navarasu/onedark.nvim",
-    { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
-    "nyoom-engineering/oxocarbon.nvim",
-    "oonamo/ef-themes.nvim",
-    { "kepano/flexoki-neovim",     name = "flexoki" },
-    {
-        "olimorris/onedarkpro.nvim",
-        priority = 1000, -- Ensure it loads first
-    },
     {
         "Verf/deepwhite.nvim",
         lazy = false,
         priority = 1000,
     },
-    {
-        "deparr/tairiki.nvim",
-        lazy = false,
-        priority = 1000, -- only necessary if you use tairiki as default theme
-        config = function()
-            require("tairiki").setup {
-                style = "dark",
-            }
-        end,
-    },
-    {
-        'ribru17/bamboo.nvim',
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require('bamboo').setup {
-                -- optional configuration here
-            }
-            require('bamboo').load()
-        end,
-    },
-    {
-        "elliotekj/enough.vim"
-    },
-    { "blazkowolf/gruber-darker.nvim" }
+    "elliotekj/enough.vim",
+    "blazkowolf/gruber-darker.nvim"
 }
