72cf646564d842d5f0e6c910646f2276ff631d71
Bastian Dabernig <bastian.dabernig@pos.ag>
2025-09-09 15:47:59 +0200

    Leftover single quotes

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -10,19 +10,19 @@ return {
 
     -- Completion
     {
-        'saghen/blink.cmp',
-        dependencies = { 'rafamadriz/friendly-snippets' },
-        version = '1.*',
-        ---@module 'blink.cmp'
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "1.*",
+        ---@module "blink.cmp"
         ---@type blink.cmp.Config
         opts = {
-            keymap = { preset = 'default' },
+            keymap = { preset = "default" },
             appearance = {
-                nerd_font_variant = 'mono'
+                nerd_font_variant = "mono"
             },
             completion = { documentation = { auto_show = false } },
             sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
+                default = { "lsp", "path", "snippets", "buffer" },
             },
             fuzzy = { implementation = "prefer_rust_with_warning" }
         },
