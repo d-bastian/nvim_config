d40f73b8680c531eddc5f008a06d0a7f77fd9244
Bastian Dabernig <bastian.dabernig@pos.ag>
2026-08-26 11:39:00 +0200

    Added csharp, docker-compose to mason ensure_installed

--- lua/customize/mason-setup.lua
+++ lua/customize/mason-setup.lua
@@ -1,7 +1,7 @@
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-    ensure_installed = { "gopls", "lua_ls", "pylsp", "jsonls", "ts_ls" },
+    ensure_installed = { "gopls", "lua_ls", "pylsp", "jsonls", "ts_ls", "csharp_ls", "docker_compose_language_service" },
     automatic_enable = true
 })
 
