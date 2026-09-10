b60be061e5167b917f6c137ada605714c65ae6d0
Bastian Dabernig <bastian.dabernig@pos.ag>
2026-09-10 09:54:21 +0200

    Added tabstop 4 for yaml filetype

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -111,3 +111,13 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         })
     end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "yaml", "yaml.ansible" },
+    callback = function()
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.softtabstop = 4
+        vim.opt_local.expandtab = true
+    end,
+})
